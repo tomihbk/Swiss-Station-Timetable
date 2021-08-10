8b9af65527c41654de59ad562b40acff2de076f1
Thomas Berhane <thomasnegassi@gmail.com>
2021-08-10 12:48:02 +0200

    init react-app with tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  // removes unused styles in production
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
