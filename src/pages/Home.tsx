87424754ec5c4b9ae9bf46d850a1796279d9993a
Thomas Berhane <thomasnegassi@gmail.com>
2021-08-24 23:27:51 +0200

    feat: add placeholder home and result pages

---
+++ src/pages/Home.tsx
@@ -0,0 +1,9 @@
+const Home = (): React.ReactElement => {
+    return (
+        <div>
+            This is home page
+        </div>
+    )
+}
+
+export default Home
---
+++ src/pages/Result.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Result = (): React.ReactElement => {
+    return (
+        <div>
+            This is the result page
+        </div>
+    )
+}
+
+export default Result
