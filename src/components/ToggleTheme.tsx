e4be1728d4d8202b7b40fd7bea201f61a7b9a9de
Thomas Berhane <thomasnegassi@gmail.com>
2021-08-25 21:04:49 +0200

    feat: add dark theme to themeToggler

--- src/components/ToggleTheme.tsx
+++ src/components/ToggleTheme.tsx
@@ -5,10 +5,10 @@ import { ReactComponent as Sun }  from '../images/sun.svg'
 const ToggleTheme = (): React.ReactElement => {
     const [colorTheme,setTheme] = useDarkMode()
     return (
-        <div onClick={ ()=> setTheme(colorTheme)}>
+        <div onClick={ ()=> setTheme(colorTheme)} className="rounded-full bg-blue-900  dark:bg-blue-200 p-3 w-12 m-2 inline-block hover:cursor-pointer">
             { colorTheme !== 'light' ? 
-            <Moon className="stroke-current text-black dark:text-white w-12 m-2" /> :
-            <Sun className="fill-current text-black dark:text-white w-12 m-2"/>
+            <Moon className="stroke-current text-blue-200 w-auto" /> :
+            <Sun className="fill-current dark:text-blue-700 w-auto"/>
             }
         </div>
     )
