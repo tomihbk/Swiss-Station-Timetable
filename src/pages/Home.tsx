da3369c86423bf764019933989a899c43d210a26
Thomas Berhane <thomasnegassi@gmail.com>
2021-08-25 21:03:03 +0200

    feat: add homepage with a list of train stations

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,19 @@
+import ModernSearchBar from "../components/ModernSearchBar"
+import SuggestedStationsList from "../components/SuggestedStationsList"
+import ToggleTheme from "../components/ToggleTheme"
+
 const Home = (): React.ReactElement => {
     return (
-        <div>
-            This is home page
+        <div className="min-h-screen bg-white dark:bg-gray-800 transition duration-500" >
+            <ToggleTheme/>
+            <h1 className="text-red-700 font-display font-black text-6xl text-center mt-10">Swiss Station Timetable</h1>
+            <p className="text-gray-600 text-lg text-center mt-4 font-secondary dark:text-gray-300 mx-2">Click on the suggested stations or use the search bar</p>
+            <div className="mt-10">
+            <SuggestedStationsList /> 
+            </div>
+            <div className="mt-15">
+               <ModernSearchBar/>
+            </div>
         </div>
     )
 }
