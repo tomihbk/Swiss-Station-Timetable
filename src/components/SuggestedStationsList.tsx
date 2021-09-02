1cc61f4d8d4db2a47ca6a81bd63ed6248ee2bfc7
Thomas Berhane <thomasnegassi@gmail.com>
2021-09-02 21:38:33 +0200

    fix: modify suggested station list

--- src/components/SuggestedStationsList.tsx
+++ src/components/SuggestedStationsList.tsx
@@ -2,28 +2,34 @@ import SuggestedStation from "./SuggestedStation";
 
 const stationsList = [
   {
-    stationName: "Lausanne",
+    stationName: "Bern",
     imageUrl:
-      "https://cdn.img.sbb.ch/content/dam/internet/sharedimages/objekte/Bahnhof-Lausanne.jpg",
-    },
-     {
+      "https://cdn.img.sbb.ch/content/dam/internet/sharedimages/objekte/Bahnhof_Bern.jpg?q=45"
+  },
+    {
     stationName: "Basel",
     imageUrl:
-      "https://cdn.img.sbb.ch/content/dam/internet/sharedimages/objekte/Bahnhof_BaselSBB.jpg",
-    },
+      "https://cdn.img.sbb.ch/content/dam/internet/sharedimages/objekte/Bahnhof_BaselSBB.jpg?q=45"
+  },
       {
-    stationName: "Fribourg",
+    stationName: "Geneva",
     imageUrl:
-      "https://cdn.img.sbb.ch/content/dam/internet/sharedimages/objekte/Bahnhof-Fribourg.jpg",
-  }, {
+      "https://cdn.img.sbb.ch/content/dam/internet/sharedimages/objekte/Bahnhof-Genf.jpg?q=45"
+  },
+  {
+    stationName: "Lausanne",
+    imageUrl:
+      "https://cdn.img.sbb.ch/content/dam/internet/sharedimages/objekte/Bahnhof-Lausanne.jpg?q=45"
+  },
+ {
     stationName: "Zürich",
     imageUrl:
-      "https://cdn.img.sbb.ch/content/dam/internet/sharedimages/objekte/Zuerich-HB.jpg",
-  },
+      "https://cdn.img.sbb.ch/content/dam/internet/sharedimages/objekte/Zuerich-HB.jpg?q=45"
+  }, 
 ];
 
 const SuggestedStationsList = (): React.ReactElement => {
-    return <div className="flex flex-wrap justify-center opacity-100 dark:opacity-90">
+    return <div className="relative flex flex-wrap justify-center opacity-100 z-10 dark:opacity-90">
         {stationsList.map((station, index) => <SuggestedStation key={ index} title={station.stationName} imageUrl={station.imageUrl} onClick={()=>alert(station.stationName)}/>)}
   </div>;
 };
