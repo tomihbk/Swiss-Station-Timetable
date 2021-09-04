96c81e5cd3c9404c3a6353c05060fb40aed743de
Thomas Berhane <thomasnegassi@gmail.com>
2021-09-04 02:33:25 +0200

    feat: add toggletheme button part of the template

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import Home from "./pages/Home";
 import Trips from "./pages/Trips";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import ToggleTheme from "./components/ToggleTheme";
 
 function App(): React.ReactElement {
   return (
     <Router>
       <div className="min-h-screen flex flex-col justify-center bg-gray-100 dark:bg-gray-800 transition duration-500" >
+       <ToggleTheme />
       <Switch>
         <Route exact path="/">
           <Home />
--- src/components/ToggleTheme.tsx
+++ src/components/ToggleTheme.tsx
@@ -5,7 +5,7 @@ import { ReactComponent as Sun } from '../images/sun.svg'
 const ToggleTheme = (): React.ReactElement => {
     const [colorTheme, setTheme] = useDarkMode()
     return (
-        <div onClick={() => setTheme(colorTheme)} className="absolute z-30 rounded-full bg-blue-900 dark:bg-blue-200 p-1 w-8 md:p-2 md:w-10 lg:w-12 m-3 inline-block hover:cursor-pointer right-0 top-0">
+        <div onClick={() => setTheme(colorTheme)} className="absolute rounded-full bg-blue-900 dark:bg-blue-200 p-1 w-8 md:p-2 md:w-10 lg:w-12 m-3 inline-block hover:cursor-pointer right-0 top-0" style={{ zIndex: 500 }}>
             {colorTheme !== 'light' ?
                 <Moon className="stroke-current text-blue-200 w-auto" /> :
                 <Sun className="fill-current dark:text-blue-700 w-auto" />
