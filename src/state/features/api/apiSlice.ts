5fbd420185f25b29e19417e54c00ca80606deb32
Thomas <thomasnegassi@gmail.com>
2023-10-18 19:28:23 +0200

    feat: store apiquery to localstorage, refreshing page is functional

--- src/state/features/api/apiSlice.ts
+++ src/state/features/api/apiSlice.ts
@@ -1,19 +1,23 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { ApiBodyTypeData } from "../../action-types";
 
-const initialState = {
-  apiQuery: {} as ApiBodyTypeData
-};
+const localStorageAPIRequest = localStorage.getItem("SWISS_TRANSPORT_API_QUERY") ?
+JSON.parse(localStorage.getItem("SWISS_TRANSPORT_API_QUERY")): {}
 
+const initialState = {
+  apiQuery: localStorageAPIRequest as ApiBodyTypeData
+};
 const apiSlice = createSlice({
   name: "apiquery",
   initialState,
   reducers: {
     addAPIQuery: (state, action) => {
       state.apiQuery = action.payload;
+      localStorage.setItem("SWISS_TRANSPORT_API_QUERY", JSON.stringify(action.payload))
     },
     updateAPIQuery: (state, action) => {
       state.apiQuery = { ...state.apiQuery, ...action.payload };
+      localStorage.setItem("SWISS_TRANSPORT_API_QUERY", JSON.stringify(state.apiQuery ))
     }
   },
 });
