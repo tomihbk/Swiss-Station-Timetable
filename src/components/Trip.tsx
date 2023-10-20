e08757dbbfb89fdfffc388d3f63c904577e72f65
Thomas <thomasnegassi@gmail.com>
2023-10-20 22:51:43 +0200

    fix: trip components are much flexible with desktop and mobile

--- src/components/Trip.tsx
+++ src/components/Trip.tsx
@@ -32,24 +32,24 @@ const Trip = ({ data }: any): React.ReactElement => {
   }
 
   return (
-    <div className={`bg-white pr-4 md:pr-0 dark:bg-gray-700  ${data.RequestedStation.IsTripCancelled && "border-2 border-red-600 border-dotted"} pt-4 pb-2 mx-auto w-5/6 md:w-4/6 flex flex-col filter drop-shadow-md rounded-xl text-sm my-5`}>
-      <div className="grid grid-cols-4 md:grid-cols-6">
+    <div className={`bg-white p-3 pl-1 pt-4 md:pb-1 md:px-0 dark:bg-gray-700 ${data.RequestedStation.IsTripCancelled && "border-2 border-red-600 border-dotted"} mx-auto w-5/6 md:w-4/6 flex flex-col filter drop-shadow-md rounded-xl text-sm my-5`}>
+      <div className="grid grid-cols-4 md:grid-cols-6 md:mb-2">
+        <div className="direction col-span-4 md:col-span-6 mx-auto mb-3 text-lg md:text-2xl font-bold font-display dark:text-white">
+          {data.StopEventResponseContext.IsItDeparture ?
+            `${data.RequestedStation.StartPoint} ${String.fromCodePoint(10230)} ${data.Destination.EndPointName}` :
+            `${data.Origin.PointName} ${String.fromCodePoint(10230)} ${data.RequestedStation.StartPoint}`}
+        </div>
         <div className="icon flex flex-col justify-center items-center">
           {/* <div className="type">
             {data.RequestedStation.TransportMethod.PtMode}
           </div> */}
           <TransportIcon type={data.RequestedStation.TransportMethod.PtMode} className="w-12 fill fill-current bg-blue-100 text-blue-600 dark:text-gray-100 dark:bg-blue-600 p-2 rounded-lg" />
-          <div className="trainnumber text-red-700 bg-red-100 dark:text-gray-100 dark:bg-red-700 px-3 rounded-full font-bold italic my-2">
+          <div className="trainnumber text-red-700 bg-red-100 dark:text-gray-100 dark:bg-red-700 px-3 rounded-full font-bold italic mt-2">
             {data.RequestedStation.PublishedLineName}
           </div>
         </div>
         <div className="info col-span-3 md:col-span-4 flex flex-col">
-          <div className="destination mx-auto text-lg md:text-2xl font-bold font-display dark:text-white">
-            {data.StopEventResponseContext.IsItDeparture ?
-              `${data.RequestedStation.StartPoint} ${String.fromCodePoint(10230)} ${data.Destination.EndPointName}` :
-              `${data.Origin.PointName} ${String.fromCodePoint(10230)} ${data.RequestedStation.StartPoint}`}
-          </div>
-          <div className="details flex flex-row flex-wrap text-center py-3 my-3 bg-gray-100 dark:bg-gray-600 rounded-xl">
+          <div className="details flex flex-row flex-wrap justify-center content-center sm:text-center py-3 h-full bg-gray-100 dark:bg-gray-600 rounded-xl">
             <div className="time min-w-1/2">
               {data.StopEventResponseContext.IsItDeparture ? "Departure" : "Arrival"} :{" "}
               <span className="font-semibold"> {moment(data.StopEventResponseContext.IsItDeparture ?
@@ -76,13 +76,13 @@ const Trip = ({ data }: any): React.ReactElement => {
             </div>
           </div>
         </div>
-        {!data.RequestedStation.IsTripCancelled && data.StopEventResponseContext.IsItDeparture && <div className={`status col-span-4 md:col-span-1 flex flex-col justify-center items-center ${deltaBetweenTimetableEstimatedDeparture > 0 ? 'text-red-500' : 'text-green-500'} font-bold text-base`}>
+        {!data.RequestedStation.IsTripCancelled && data.StopEventResponseContext.IsItDeparture && <div className={`status col-span-4 md:col-span-1 flex flex-col justify-center items-center mt-3 md:mt-0 ${deltaBetweenTimetableEstimatedDeparture > 0 ? 'text-red-500' : 'text-green-500'} font-bold text-base`}>
           {deltaBetweenTimetableEstimatedDeparture > 0 ? `+${deltaBetweenTimetableEstimatedDeparture} min` : 'On Time'}
         </div>}
-        {!data.RequestedStation.IsTripCancelled && !data.StopEventResponseContext.IsItDeparture && <div className={`status col-span-4 md:col-span-1 flex flex-col justify-center items-center ${deltaBetweenTimetableEstimatedArrival > 0 ? 'text-red-500' : 'text-green-500'} font-bold text-base`}>
+        {!data.RequestedStation.IsTripCancelled && !data.StopEventResponseContext.IsItDeparture && <div className={`status col-span-4 md:col-span-1 flex flex-col justify-center items-center mt-3 md:mt-0${deltaBetweenTimetableEstimatedArrival > 0 ? 'text-red-500' : 'text-green-500'} font-bold text-base`}>
           {deltaBetweenTimetableEstimatedArrival > 0 ? `+${deltaBetweenTimetableEstimatedArrival} min` : 'On Time'}
         </div>}
-        {data.RequestedStation.IsTripCancelled && <div className={"status col-span-4 md:col-span-1 flex flex-col justify-center items-center text-red-500 font-bold text-base"}>
+        {data.RequestedStation.IsTripCancelled && <div className={"status col-span-4 md:col-span-1 flex flex-col justify-center items-center text-red-500 font-bold text-base mt-3 md:mt-0"}>
           {'Cancelled'}
         </div>}
       </div>
