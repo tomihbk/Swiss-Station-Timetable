6be859329a5fc152cbdff6fec47949916aee661a
Thomas <thomasnegassi@gmail.com>
2023-10-25 10:22:36 +0200

    feat: add border to suggested stations list

--- src/components/SuggestedStation.tsx
+++ src/components/SuggestedStation.tsx
@@ -8,7 +8,7 @@ interface SuggestedStationPropType {
 
 const SuggestedStation: React.FC<SuggestedStationPropType> = ({ title, imageUrl, onClick }): React.ReactElement => {
     return (
-        <div className="flex flex-wrap flex-col rounded-2xl w-auto min-w-min-suggested-station-card h-28 md:h-48 m-5 justify-end hover:cursor-pointer transform-gpu hover:scale-105 transition duration-200 ease-in-out" onClick={onClick}>
+        <div className="flex flex-wrap flex-col w-auto min-w-min-suggested-station-card h-28 md:h-48 m-5 justify-end hover:cursor-pointer transform-gpu hover:scale-105 ease-in-out rounded-2xl dark:ring-1 transition duration-300 scale-100 ring-gray-700" onClick={onClick}>
             <img alt={title} className="absolute -z-10 h-full w-full rounded-2xl object-cover loaded" src={imageUrl}/>
             <div className="absolute inset-0 -z-10 bg-gradient-to-t from-gray-900 via-gray-900/20 rounded-2xl"></div>
             <h3 className={`text-gray-100 font-display text-2xl md:text-4xl font-bold w-full p-3 md:p-4`}>{title}</h3>
