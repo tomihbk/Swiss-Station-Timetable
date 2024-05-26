ebb4eaf149a5cc3d2a352f0e9636d22fc7c16d7d
Thomas <thomasnegassi@gmail.com>
2024-05-26 19:09:23 +0200

    feat: add more refined tests for SearchForm Comp

--- src/components/__tests__/SearchForm.test.tsx
+++ src/components/__tests__/SearchForm.test.tsx
@@ -1,27 +1,30 @@
-import { screen } from "@testing-library/react"
+import { screen, waitFor } from "@testing-library/react"
 import userEvent from '@testing-library/user-event'
 import '@testing-library/jest-dom'
 import renderWithContext from "../../util/test-utils"
-import SearchForm, { getAllStations } from "../SearchForm"
+import SearchForm from "../SearchForm"
 
 describe("SearchForm Component", () => {
 
-    test("render 5 fields with 1 submit button", () => {
+    test("render 5 fields with 1 submit button", async () => {
+
         renderWithContext(<SearchForm />)
 
-        const stationInput = screen.getByLabelText("station name")
-        const timeInput = screen.getByLabelText("time")
-        const dateInput = screen.getByLabelText("date")
-        const directionInput = screen.getByLabelText("direction")
-        const resultInput = screen.getByLabelText("result quantity")
-        const submitInput = screen.getByDisplayValue("Search")
+        await waitFor(() => {
+            const stationInput = screen.getByLabelText("station name")
+            const timeInput = screen.getByLabelText("time")
+            const dateInput = screen.getByLabelText("date")
+            const directionInput = screen.getByLabelText("direction")
+            const resultInput = screen.getByLabelText("result quantity")
+            const submitInput = screen.getByDisplayValue("Search")
 
-        expect(stationInput).toBeInTheDocument()
-        expect(timeInput).toBeInTheDocument()
-        expect(dateInput).toBeInTheDocument()
-        expect(directionInput).toBeInTheDocument()
-        expect(resultInput).toBeInTheDocument()
-        expect(submitInput).toBeInTheDocument()
+            expect(stationInput).toBeInTheDocument()
+            expect(timeInput).toBeInTheDocument()
+            expect(dateInput).toBeInTheDocument()
+            expect(directionInput).toBeInTheDocument()
+            expect(resultInput).toBeInTheDocument()
+            expect(submitInput).toBeInTheDocument()
+        });
     })
 
     it("should fail if submitting without filling fields", () => {
@@ -32,37 +35,31 @@ describe("SearchForm Component", () => {
 
         const user = userEvent.setup()
 
-        const submitInput = screen.getByDisplayValue("Search")
-        user.click(submitInput)
+        const submitButton = screen.getByDisplayValue("Search")
+        user.click(submitButton)
 
         expect(directionInput).toBeInvalid()
         expect(stationInput).toBeInvalid()
         expect(resultInput).toBeInvalid()
     })
 
-    test('fetch API should be running', async () => {
-        renderWithContext(<SearchForm />)
-        
-    })
 
-    it("should pass if submitting with filling fields", async () => {
+    it("should pass if submitting with prefilled fields", async () => {
+
         renderWithContext(<SearchForm />)
+
         const user = userEvent.setup()
 
         const stationInput = screen.getByLabelText("station name")
         const directionInput = screen.getByLabelText("direction")
         const resultInput = screen.getByLabelText("result quantity")
 
-
-        await user.type(stationInput, "Bulle")
+        await user.type(stationInput, "Aadorf")
         await user.selectOptions(directionInput, "arrival")
         await user.selectOptions(resultInput, "10")
 
-        const submitInput = screen.getByDisplayValue("Search")
-        user.click(submitInput)
-
-        expect(directionInput).toBeValid()
         expect(stationInput).toBeValid()
+        expect(directionInput).toBeValid()
         expect(resultInput).toBeValid()
     })
 })
